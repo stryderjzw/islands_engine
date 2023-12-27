c4c0918a7550e22aae77f378f36d6f569d77977e
Justin Wong <stryderjzw@gmail.com>
2023-12-27 15:50:35 -0800

    add functions to guesses

--- lib/islands_engine/guesses.ex
+++ lib/islands_engine/guesses.ex
@@ -1,5 +1,5 @@
 defmodule IslandsEngine.Guesses do
-  alias __MODULE__
+  alias IslandsEngine.{Coordinate, Guesses}
 
   @enforce_keys [:hits, :misses]
   defstruct [:hits, :misses]
@@ -7,4 +7,10 @@ defmodule IslandsEngine.Guesses do
   def new() do
     %Guesses{hits: MapSet.new(), misses: MapSet.new()}
   end
+
+  def add(%Guesses{} = guesses, :hit, %Coordinate{} = coordinate),
+    do: update_in(guesses.hits, &MapSet.put(&1, coordinate))
+
+  def add(%Guesses{} = guesses, :miss, %Coordinate{} = coordinate),
+    do: update_in(guesses.misses, &MapSet.put(&1, coordinate))
 end
