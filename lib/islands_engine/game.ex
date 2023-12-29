3b744376fe09bb50f233a2c4cf12c00b7198ef28
Justin Wong <stryderjzw@gmail.com>
2023-12-28 23:43:33 -0800

    implemented guess_coordinate

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -23,6 +23,9 @@ defmodule IslandsEngine.Game do
   def set_islands(game, player) when player in @players,
     do: GenServer.call(game, {:set_islands, player})
 
+  def guess_coordinate(game, player, row, col) when player in @players,
+    do: GenServer.call(game, {:guess_coordinate, player, row, col})
+
   def handle_call({:add_player, name}, _from, state_data) do
     with {:ok, rules} <- Rules.check(state_data.rules, :add_player) do
       state_data
@@ -73,6 +76,30 @@ defmodule IslandsEngine.Game do
     end
   end
 
+  def handle_call({:guess_coordinate, player, row, col}, _from, state_data) do
+    player_key = player
+    opponent_key = opponent(player_key)
+    opponent_board = player_board(state_data, opponent_key)
+
+    with {:ok, rules} <-
+           Rules.check(state_data.rules, {:guess_coordinate, player_key}),
+         {:ok, coordinate} <-
+           Coordinate.new(row, col),
+         {hit_or_miss, forested_island, win_status, opponent_board} <-
+           Board.guess(opponent_board, coordinate),
+         {:ok, rules} <-
+           Rules.check(rules, {:win_check, win_status}) do
+      state_data
+      |> update_board(opponent_key, opponent_board)
+      |> update_guesses(player_key, hit_or_miss, coordinate)
+      |> update_rules(rules)
+      |> reply_success({hit_or_miss, forested_island, win_status})
+    else
+      :err -> {:reply, :err, state_data}
+      {:err, :invalid_coordinate} -> {:reply, {:err, :invalid_coordinate}, state_data}
+    end
+  end
+
   defp player_board(state_data, player), do: Map.get(state_data, player).board
 
   defp update_board(state_data, player, board) do
@@ -83,5 +110,14 @@ defmodule IslandsEngine.Game do
 
   defp update_rules(state_data, rules), do: %{state_data | rules: rules}
 
+  defp update_guesses(state_data, player, hit_or_miss, coordinate) do
+    update_in(state_data[player].guesses, fn guesses ->
+      Guesses.add(guesses, hit_or_miss, coordinate)
+    end)
+  end
+
   defp reply_success(state_data, reply), do: {:reply, reply, state_data}
+
+  defp opponent(:player1), do: :player2
+  defp opponent(:player2), do: :player1
 end
