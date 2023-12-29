22879b0fcbf86993adad730e2ab5670360c1e24d
Justin Wong <stryderjzw@gmail.com>
2023-12-28 22:42:59 -0800

    fix error code on island; add position_island functionality for game

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -1,7 +1,9 @@
 defmodule IslandsEngine.Game do
   use GenServer
 
-  alias IslandsEngine.{Board, Guesses, Rules}
+  alias IslandsEngine.{Board, Coordinate, Guesses, Island, Rules}
+
+  @players [:player1, :player2]
 
   def start_link(name) when is_binary(name) do
     GenServer.start_link(__MODULE__, name, [])
@@ -17,6 +19,10 @@ defmodule IslandsEngine.Game do
     GenServer.call(game, {:add_player, name})
   end
 
+  def position_island(game, player, key, row, col) when player in @players do
+    GenServer.call(game, {:position_island, player, key, row, col})
+  end
+
   def handle_call({:add_player, name}, _from, state_data) do
     with {:ok, rules} <- Rules.check(state_data.rules, :add_player) do
       state_data
@@ -26,6 +32,34 @@ defmodule IslandsEngine.Game do
     else
       :err -> {:reply, :err, state_data}
     end
+  end
+
+  def handle_call({:position_island, player, key, row, col}, _from, state_data) do
+    board = player_board(state_data, player)
+
+    with {:ok, rules} <-
+           Rules.check(state_data.rules, {:position_islands, player}),
+         {:ok, coordinate} <-
+           Coordinate.new(row, col),
+         {:ok, island} <-
+           Island.new(key, coordinate),
+         %{} = board <-
+           Board.position_island(board, key, island) do
+      state_data
+      |> update_board(player, board)
+      |> update_rules(rules)
+      |> reply_success(:ok)
+    else
+      :err -> {:reply, :err, state_data}
+      {:err, :invalid_coordinate} -> {:reply, {:err, :invalid_coordinate}, state_data}
+      {:err, :invalid_island_type} -> {:reply, {:err, :invalid_island_type}, state_data}
+    end
+  end
+
+  defp player_board(state_data, player), do: Map.get(state_data, player).board
+
+  defp update_board(state_data, player, board) do
+    Map.update!(state_data, player, fn player -> %{player | board: board} end)
   end
 
   defp update_player2_name(state_data, name), do: put_in(state_data.player2.name, name)
--- lib/islands_engine/island.ex
+++ lib/islands_engine/island.ex
@@ -50,5 +50,5 @@ defmodule IslandsEngine.Island do
   defp offsets(:dot), do: [{0, 0}]
   defp offsets(:l_shape), do: [{0, 0}, {1, 0}, {2, 0}, {2, 1}]
   defp offsets(:s_shape), do: [{0, 0}, {0, 2}, {1, 0}, {1, 1}]
-  defp offsets(_), do: {:error, :invalid_island_type}
+  defp offsets(_), do: {:err, :invalid_island_type}
 end
